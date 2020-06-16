17a0b112a37cec049e42ed29e08feaa6b71e1e9c
Dean Erickson <dmepilot@yahoo.com>
2020-06-16 16:17:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
 class Dog
   def name= (dog_name) #name= is a "setter" method
-    this_dogs_name = dog_name
+    @this_dogs_name = dog_name
   end
   
   def name #name is a "getter" method 
-    this_dogs_name
+    @this_dogs_name
   end
   
   lassie = Dog.new
